bac9756c351a7c506c87363f2cfda386d3567ac2
IvanTheFox <ivan.shepelev.06@inbox.ru>
2025-12-17 02:23:40 +0300

    first commit

---
+++ src/test/java/com/not_a_team/university/UniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.not_a_team.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
